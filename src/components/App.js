4b02771e824edaeac8419d33672650569a1c91e0
dzewelina <ewelina.zukowska26@gmail.com>
2018-02-28 10:54:23 +0000

    Add Hotels component - display the data

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import '../App.css';
+import Hotels from './Hotels';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Working!
+        <header>
+          <h1>HOTEL SEARCHING WEBSITE</h1>
+        </header>
+        <Hotels />
       </div>
     );
   }
---
+++ src/components/Hotels.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import hotels from '../data/hotels.json';
+
+class Hotels extends Component {
+  state = {
+    hotels
+  }
+
+  render() {
+    const { hotels } = this.state;
+    return (
+      <div>
+        {hotels.map((hotel, i) => (
+          <div key={i}>
+            <h1>{hotel.Name}</h1>
+            <p>
+              {hotel.Facilities.map((facility, i) => (<span key={i}>{facility} </span>))}
+            </p>
+            <img src={hotel.Image} alt={`${hotel.Name}`} />
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Hotels;
