e3840ff40117b49ef813fb6e79aecbefd75f274f
dzewelina <ewelina.zukowska26@gmail.com>
2018-02-28 13:23:47 +0000

    Add star rating to Hotel component

--- src/components/Hotel.js
+++ src/components/Hotel.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import propTypes from 'prop-types';
 
+
+
 const Hotel = ({ hotel }) => (
   <li className='hotel media'>
     <img className='align-self-start mr-3' src={hotel.Image} alt={`${hotel.Name}`} />
     <div className='media-body'>
       <h1 className='mt-0'>{hotel.Name}</h1>
+      <span>{hotel.StarRating}<i className="fas fa-star"></i></span>
       <p>
         {hotel.Facilities.map((facility, i) => (<span key={i}>{facility} </span>))}
       </p>
@@ -13,7 +16,8 @@ const Hotel = ({ hotel }) => (
   </li>
 );
 
-Hotel.propTypes ={
+
+Hotel.propTypes = {
   hotel: propTypes.object.isRequired
 };
 
